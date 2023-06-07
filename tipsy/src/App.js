5d041d03a386656c9ea9d1854674fb95a02fc8d2
jihwan <guswlghks007@naver.com>
2023-06-07 17:43:32 +0900

    서버와 연결 및 상품 수량 개수 카운트 후 표시

--- tipsy/src/App.js
+++ tipsy/src/App.js
@@ -2,8 +2,23 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+    const [user_ID, setUser_ID] = useState();
+    const [user_password, setUser_password] = useState();
+
+
+
+    useEffect(() => {
+        axios.get("")
+            .then(() => { })
+            .catch(() => { })
+    })
+
+
     return (
         <>
             <div className="App">
